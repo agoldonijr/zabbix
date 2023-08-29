293f58ab49a941b16a4397305e11629e1c9d3d85
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2023-08-29 15:22:56 -0300

    Install Zabbix Agent 2

--- installZabbixClientRHEL8.sh
+++ installZabbixClientRHEL8.sh
@@ -7,8 +7,10 @@ if [ -f /etc/yum.repos.d/epel.repo ]; then
         echo "excludepkgs=zabbix*" >>  /etc/yum.repos.d/epel.repo
 fi
 
+echo "Adicionando repositório"
 rpm -Uvh https://repo.zabbix.com/zabbix/6.5/oracle/9/x86_64/zabbix-release-6.5-2.el9.noarch.rpm > /dev/null 2>&1
 dnf clean all > /dev/null 2>&1
+echo "Instalando pacotes zabbix-agent2 zabbix-agent2-plugin-postgresql "
 dnf install -y zabbix-agent2 zabbix-agent2-plugin-postgresql > /dev/null 2>&1 
 
 systemctl enable zabbix-agent2 --now
